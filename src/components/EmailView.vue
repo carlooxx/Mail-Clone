f18d4586ef37f74a4b18e008272b4bf5ac2de750
Karlo Braovac <karlo.braovac@gmail.com>
2023-01-31 13:37:33 +0100

    Action Bar

--- src/components/EmailView.vue
+++ src/components/EmailView.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="email-display">
+        <div>
+            <button>Archive</button>
+            <button @click="toggleRead">{{ email.read ? 'Mark as Unread (r)' : 'Mark as read (r)' }}</button>
+            <button>Newer</button>
+            <button>Older</button>
+        </div>
         <h2 class="mb-0">Subject: <strong>{{ email.subject }}</strong></h2>
         <div><em>From {{ email.from }} on {{ format(new Date(email.sentAt), 'MMM do yyyy')}}</em></div>
         <div v-html="marked(email.body)" />
@@ -8,19 +14,28 @@
 
 <script>
 import { format } from 'date-fns'
-import {marked} from 'marked'
+import { marked } from 'marked'
+import axios from 'axios'
+import useKeydown from './composables/use-keydown'
 
     export default {
-        setup() {
-            return {
-                format,
-                marked
-            }
-        },
         props: {
             email: {
                 type: Object,
                 required: true
+            }
+        },
+        setup(props) {
+            let toggleRead = () => {
+            let { email } = props
+                email.read = !email.read
+                axios.put(`http://localhost:3000/emails/${email.id}`, email)
+            }
+            useKeydown([{key: 'r', fn: () => { toggleRead() }}])
+            return {
+                format,
+                marked,
+                toggleRead
             }
         }
     }
